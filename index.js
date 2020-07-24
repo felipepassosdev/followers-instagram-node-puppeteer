4ee0d424ec17cc5f7a23d7bdd81fa512607faaae
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-07-24 17:29:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,7 @@
 var express = require('express');
 var app = express();
 
-//... your code here ...
-                                
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
-});
+app.get("/", (req, res) => {
+    res.send("hello, world")
+})
+app.listen(3000, () => console.log("deu certo porra"))
