221b5b48aa662f45aef43be69b96f18b2def15d7
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-07-24 17:31:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello, world")
+    res.send("<h1>Juju gatinha, to com saudades</h1>")
 })
 app.listen(3000, () => console.log("deu certo porra"))
