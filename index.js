5b43beccc2ce9fd18d910486c8ba8a6ba0bbc9c2
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-07-24 17:11:37 -0300

    att

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 
-const start = async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.instagram.com/filipedeschamps/followers/')
-}
+// const start = async () => {
+//     const browser = await puppeteer.launch() //executar
+//     const page = await browser.newPage() // nova pag
+//     await page.goto('https://www.instagram.com/filipedeschamps/followers/') // va para esse site
+// }
+var express = require('express');
+var app = express();
+
+//... your code here ...
+                                
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
